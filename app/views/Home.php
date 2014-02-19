b3bd3a835de408b75dd545b06691bece9acf18ff
Sanjay Thomas Alanoly <sanjay@sanjay.com>
2014-02-19 13:56:30 +0530

    Tracking updated

--- app/views/Home.php
+++ app/views/Home.php
@@ -16,8 +16,18 @@
 	<script src="js/bootstrap.min.js"></script>	
 	<script src="js/Home.js" type="text/javascript"></script>
 
-	<?php include_once("analyticstracking.php") ?>
+	
 
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-48212155-1', 'makeadiff.in');
+      ga('send', 'pageview');
+
+    </script>
 
     <script type="text/javascript" src="https://www.google.com/jsapi"></script>
     <script type="text/javascript">
