684059db920d70ef85f1ad4e7b438d28e95923f2
Sanjay Thomas Alanoly <sanjay@sanjay.com>
2014-02-21 19:47:43 +0530

    Timing change

--- public/js/WarRoom.js
+++ public/js/WarRoom.js
@@ -205,7 +205,7 @@ function init(){
 		    // find the amount of "seconds" between now and target
 			var now = new Date();
 
-			var start_time = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 19,55,0).getTime();
+			var start_time = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 20,00,0).getTime();
 			var end_time = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 20,20,0).getTime();
 
 			var tom_start_time = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 12,15,0).getTime();
