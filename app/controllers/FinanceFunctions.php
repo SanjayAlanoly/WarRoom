ccb30a70973bda1483ddca95e49bcb20d6676fef
Sanjay Thomas Alanoly <sanjay@sanjay.com>
2014-04-22 12:07:17 +0530

    Corrections

--- app/controllers/FinanceFunctions.php
+++ app/controllers/FinanceFunctions.php
@@ -12,28 +12,30 @@ class FinanceFunctions extends BaseController
 
         $data = json_decode($content);
 
-        $row = DB::connection('cfrapp')->select('SELECT TOP 1 FROM events WHERE id = ?',array($data->event_id));
+        $row = DB::connection('cfrapp')->select('SELECT id FROM events WHERE id = ? LIMIT 1',array($data->event_id));
 
         $city = DB::connection('cfrapp')->select('SELECT id,state_id FROM cities WHERE name = ?',array($data->event_city));
 
-        if (empty($city)) {
-            $city->id = 25;
-            $city->state_id = 7;
+        if (empty($city[0])) {
+            $city[0] = new stdClass();
+            $city[0]->id = 25;
+            $city[0]->state_id = 7;
         }
 
-        if (empty($row)) {
+        if (empty($row[0])) {
 
             DB::connection('cfrapp')->insert("INSERT INTO events (id, event_name, image_url, ticket_price, description,
               date_range_from, date_range_to, venue_address, venue_address1, city_id, state_id, created_at, updated_at)
               VALUES(?, ?,  '', ?, ?, ?, ?, ?, NULL, ?, ?, NOW(), NOW())",
               array((int)$data->event_id, $data->event_name, $data->ticket_price, $data->event_descp,
-              $data->event_start_date, $data->event_end_date, $data->venue_address, $city->id, $city->state_id));
+              $data->event_start_date, $data->event_end_date, $data->venue_address, $city[0]->id, $city[0]->state_id));
 
         } else {
             DB::connection('cfrapp')->update('UPDATE events SET event_name = ?, ticket_price = ?, description = ?,
-              date_range_from = ?, date_range_to = ?, venue_address = ?, city_id = ?, state_id = ?,updated_at = NOW()',
+              date_range_from = ?, date_range_to = ?, venue_address = ?, city_id = ?, state_id = ?,updated_at = NOW()
+              WHERE id =?',
               array($data->event_name, $data->ticket_price, $data->event_descp, $data->event_start_date,
-              $data->event_end_date, $data->venue_address, $city->id, $city->state_id));
+              $data->event_end_date, $data->venue_address, $city[0]->id, $city[0]->state_id, $row[0]->id));
         }
 
 
